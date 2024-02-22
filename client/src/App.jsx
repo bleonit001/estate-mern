8626879172f706c01412aa705a68e8b20a4e123f
bleonit001 <bleonit001@gmail.com>
2024-02-23 00:51:40 +0100

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { BrowserRouter , Routes , Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
 
-function App() {
-  return (
-    <div color='red'>App</div>
+
+export default function App() {
+  return(
+   <BrowserRouter>
+  <Routes>
+  
+  <Route path="/" element={<Home />} />
+  <Route path="/sign-in" element={<SignIn />} />
+  <Route path="/sign-up" element={<SignUp />} />
+  <Route path="/about" element={<About />} />
+  <Route path="/profile" element={<Profile />} />
+
+  </Routes>
+  
+  </BrowserRouter>
   )
+  
 }
 
-export default App
