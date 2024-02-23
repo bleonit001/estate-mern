560092ff5dfda6807ffaa89fe9676d4628f1f96b
bleonit001 <bleonit001@gmail.com>
2024-02-23 01:28:06 +0100

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
+import Header from "./Components/Header";
 
 
 export default function App() {
   return(
    <BrowserRouter>
+   <Header/>
   <Routes>
   
   <Route path="/" element={<Home />} />
