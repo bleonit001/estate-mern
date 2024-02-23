ffefbf1b7e689f635a2aa19334e76d36d574cc1c
bleonit001 <bleonit001@gmail.com>
2024-02-23 02:09:50 +0100

    Database Connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoonse from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoonse.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB!');
+}).catch((err) => {
+    console.log(err);
+});
 
 
 const app = express();
