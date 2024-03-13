69a70e61beb330d38e72279e0dc96136cf6454c6
bleonit001 <bleonit001@gmail.com>
2024-03-14 00:18:17 +0100

    add reduc tool kit

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,11 +1,15 @@
 import { Link ,useNavigate} from 'react-router-dom';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart ,signInSuccess,signInFailure } from '../redux/user/userSlice';
+
+
 
 export default function SignIn() {
   const [formData ,setFormData] =useState({});
-  const [error,setError] =useState(null);
-  const[loading,setLoading] = useState(false);
-const navigate =useNavigate();
+  const { loading,error} = useSelector((state) => state.user);
+  const navigate =useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e)=>{
     setFormData({
       ...formData,
@@ -15,7 +19,7 @@ const navigate =useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -26,18 +30,16 @@ const navigate =useNavigate();
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/')
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
+  
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
