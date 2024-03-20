5f2b365e0cc5130e9d8662a4a6215186eb2c2e3b
bleonit001 <bleonit001@gmail.com>
2024-03-21 00:23:19 +0100

    image slider to listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from "./Pages/Profile";
 import Header from "./Components/Header";
 import PrivateRoute from "./Components/PrivateRoute";
 import UpdateListing from "./Pages/UpdateListing";
+import Listing from "./Pages/Listing";
 
 
 export default function App() {
@@ -20,6 +21,7 @@ export default function App() {
   <Route path="/sign-in" element={<SignIn />} />
   <Route path="/sign-up" element={<SignUp />} />
   <Route path="/about" element={<About />} />
+  <Route path="/listing/:listingId" element={<Listing />} />
   <Route element={<PrivateRoute/>}>
   <Route path="/profile" element={<Profile />} />
   <Route path="/create-listing" element={<CreateListing/>} />
