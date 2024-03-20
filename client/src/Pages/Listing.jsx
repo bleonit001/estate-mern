37b5caea767696749be55d338f8dab5499eff860
bleonit001 <bleonit001@gmail.com>
2024-03-21 00:30:37 +0100

    .

--- client/src/Pages/Listing.jsx
+++ client/src/Pages/Listing.jsx
@@ -89,13 +89,14 @@ export default function Listing() {
             </p>
           )}
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
-            <p className='text-2xl font-semibold'>
-              {listing.name} - ${' '}
-              {listing.offer
-                ? listing.discountPrice.toLocaleString('en-US')
-                : listing.regularPrice.toLocaleString('en-US')}
-              {listing.type === 'rent' && ' / month'}
-            </p>
+          <p className='text-2xl font-semibold'>
+  {listing.name} - {' '}
+  {listing.offer
+    ? listing.discountPrice.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
+    : listing.regularPrice.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}
+  {listing.type === 'rent' && ' / month'}
+</p>
+
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
               <FaMapMarkerAlt className='text-green-700' />
               {listing.address}
@@ -106,7 +107,7 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice} OFF
+                  ${+listing.regularPrice - +listing.discountPrice} Off
                 </p>
               )}
             </div>
