57ab6a0ae455e9de03abd1c5afa12f08e67c195b
bleonit001 <bleonit001@gmail.com>
2024-03-21 01:51:23 +0100

    add pre installetion

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from"./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import  path  from "path";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,7 +14,9 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 });
 
-///
+const __dirname =path.resolve();
+
+
 const app = express();
 
 app.use(express.json());
@@ -30,6 +33,12 @@ app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*' , (req,res,next) =>{
+    res.sendFile(path.join(__dirname,'client' ,'dist','index.html'));
+    
+})
 
 app.use((err, req, res, next) =>{
 const statusCode =err.statusCode || 500;
