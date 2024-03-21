cc5f6c4f627a1d5f087032bac736439232db0403
bleonit001 <bleonit001@gmail.com>
2024-03-21 02:45:52 +0100

    ..

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
+
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import userRouter from './routes/user.routes.js'
 import path from 'path';
 dotenv.config();
 
